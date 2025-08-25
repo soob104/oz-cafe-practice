76d6ca05fbcb6d392c15a19967c71aaa20ff862e
GuSooBin <soobddg@gmail.com>
2025-08-25 14:30:29 +0900

    react 14일차 과제

---
+++ src/Context/menuContext.jsx
@@ -0,0 +1,15 @@
+import {createContext, useContext,useState} from "react";
+import data from "../assets/data";
+
+const menuContext = createContext()
+
+export function MenuProvider({children}){
+  return(
+    <menuContext.Provider value={{menu: data.menu}}>
+      {children}
+    </menuContext.Provider>
+  )
+}
+export function useMenu () {
+return useContext(menuContext) 
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { MenuProvider } from "./Context/menuContext.jsx";
+import { CartProvider } from "./Context/cartContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+
+  <MenuProvider>
+    <CartProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </CartProvider>
+  </MenuProvider>
+
+
 );
